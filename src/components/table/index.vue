1763dd81413a9258db626a35503e2ff566ebbb4e
xugaobo <xugaobo@jd.com>
2017-11-13 11:40:34 +0800

    优化表格组件

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-table :data="tableData" border style="width: 100%">
+    <el-table :data="dataSource" border style="width: 100%">
         <el-table-column v-for="(item, index) in  columns" :key="index" :prop="item.prop" :label="item.label" :width="item.width">
         </el-table-column>
         <slot name="operate"></slot>
@@ -13,41 +13,18 @@
                 default: function () {
                     return []
                 }
+            },
+            dataSource: {
+                type: Array,
+                default: function() {
+                    return []
+                }
             }
         },
         methods: {
         },
         data() {
             return {
-                tableData: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }]
             }
         }
     }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -75,6 +75,35 @@
                     prop: 'zip',
                     label: '邮编',
                     width: 180
+                }],
+                dataSource: [{
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
                 }]
             }
         },
