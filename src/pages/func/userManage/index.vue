c7510c26522b7aa9025aa656350b5589d5a5d579
xugaobo <xugaobo@jd.com>
2018-01-07 13:44:50 +0800

    添加用户管理功能

--- src/pages/func/userManage/index.vue
+++ src/pages/func/userManage/index.vue
@@ -1,9 +1,48 @@
 <template>
-    <div>hello!</div>
+    <Layout>
+        <div slot="operate">
+            <el-button @click="createRecord" size="small">新增</el-button>
+            <el-button @click="exportTable" size="small">导出</el-button>
+        </div>
+        <TableInfo :columns="columns" :dataSource="dataSource" slot="content">
+            <p slot="operate">
+                <el-table-column label="操作" width="150">
+                    <template slot-scope="scope">
+                        <el-button @click="updateRecord(scope.row)" type="text" size="small">修改</el-button>
+                        <el-button @click="showPopover(scope.row.id)" type="text" size="small">重置密码</el-button>
+                    </template>
+                </el-table-column>
+            </p>
+        </TableInfo>
+        <FormModal :dataSource="formData" :initValue="initValue" @closeDialog="closeDialog" @confirm="confirm" :tableName="TABLE_NAME" :visible="visible" slot="form-modal" />
+    </Layout>
 </template>
 <script>
+    import TableInfo from 'Components/table/index';
+    import Layout from '../layout/index';
+    import mixin from '../mixins/tableMixins';
+    import FormModal from 'Components/formModal/index';
+    const TABLE_NAME = 'letterRecord';
     export default {
-
+        mixins: [mixin],
+        components: {
+            TableInfo,
+            FormModal,
+            Layout
+        },
+        data() {
+            return {
+                TABLE_NAME
+            }
+        },
+        mounted () {
+            this.tableName = TABLE_NAME;
+        },
+        methods: {
+            showPopover(id) {
+                this.queryLetterTable(id);
+            }
+        }
     }
 
 </script>
