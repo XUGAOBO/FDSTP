b54f56c70d884ba72bb31b6c533adc7baecdbcaa
xugaobo <xugaobo@jd.com>
2018-01-13 17:01:20 +0800

    添加欢迎页

--- src/pages/welcome/index.vue
+++ src/pages/welcome/index.vue
@@ -2,13 +2,13 @@
     <div class="welcome">
         <img src="../../assets/images/bg.png" />
         <footer>
-            <div>{{content.content}}</div>
+            <div class="welcome-period">{{content.content}}</div>
             <div class="welcome-support">
                 <h3>系统技术支持</h3>
-                <div><span>联系电话: </span> {{content.phone}}</div>
-                <div><span>QQ: </span> {{content.qq}}</div>
-                <div><span>email: </span> {{content.email}}</div>
-                <div><span>微信: </span> {{content.wechat}}</div>
+                <div v-if="content.phone"><span>联系电话: </span> {{content.phone}}</div>
+                <div v-if="content.qq"><span>QQ: </span> {{content.qq}}</div>
+                <div v-if="content.email"><span>email: </span> {{content.email}}</div>
+                <div v-if="content.wechat"><span>微信: </span> {{content.wechat}}</div>
             </div>
         </footer>
     </div>
